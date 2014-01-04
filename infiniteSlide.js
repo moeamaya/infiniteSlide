d333b100a5a08bd482b9cab81e474343e66eaffe
moeamaya <moeamaya@gmail.com>
2014-01-04 11:06:38 -0700

    pushing new fixes for long images

---
+++ infiniteSlide.js
@@ -0,0 +1,20 @@
+//
+// infiniteSlide.js
+// https://github.com/moeamaya/infiniteSlide
+// MIT licensed
+//
+// version 0.1 - 01/01/2013
+
+;(function($, doc, win) {
+  "use strict";
+
+  $.fn.infiniteSlide = function(options) {
+
+    var defaults = $.extend({
+      "speed": 700,
+      "autoscroll": false,
+    }, options );
+
+  };
+
+}(jQuery, document, window));
