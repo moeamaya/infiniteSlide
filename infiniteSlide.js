358e87c3ec3bb43497d704734398429b624ef4ca
moeamaya <moeamaya@gmail.com>
2014-01-05 23:17:34 -0700

    first code commit

--- infiniteSlide.js
+++ infiniteSlide.js
@@ -8,13 +8,34 @@
 ;(function($, doc, win) {
   "use strict";
 
-  $.fn.infiniteSlide = function(options) {
+  var name = "js-infiniteSlide";
 
-    var defaults = $.extend({
+  var InfiniteSlide = function(el, opts) {
+    var self = Object.create(InfiniteSlide.prototype);
+    self.$el = $(el);
+    self.$el.data(name, self);
+
+    self.defaults = {
       "speed": 700,
-      "autoscroll": false,
-    }, options );
+      "autoscroll": false
+    };
 
+    var meta = self.$el.data(name + "-opts");
+    self.opts = $.extend(self.defaults, opts, meta);
+
+    var init = function(){
+
+    };
+
+    init();
+    Object.freeze(self);
+    return self;
+  };
+
+  $.fn.infiniteSlide = function(options) {
+    return this.each(function() {
+      new InfiniteSlide(this, options);
+    });
   };
 
 }(jQuery, document, window));
